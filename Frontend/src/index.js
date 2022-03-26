7a748d3e379498ccd6fc6515a43fd3e0e2711235
pranav1st <pranavm14002@gmail.com>
2022-03-26 14:41:03 +0530

    Added base Consumer form

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Forms2 from './Components/consumer/Forms2';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
